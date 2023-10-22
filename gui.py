5318b49c74dd8cbba0730c515cffd91d1e9cf515
Sebastian <sebastian.braende@gmail.com>
2023-10-22 20:47:24 +0200

    Grid layout

--- gui.py
+++ gui.py
@@ -13,18 +13,16 @@ frame = ctk.CTkFrame(master=root)
 frame.pack(padx=5, pady=5, fill='both', expand=True)
 
 label = ctk.CTkLabel(master=frame, text='Login System')
-label.pack(padx=12, pady=12)
 
 button_top = ctk.CTkButton(master=frame, text="Up", command=login)
-button_top.pack(padx=12, pady=12)
 
 button_left = ctk.CTkButton(master=frame, text="Left", command=login)
-button_left.pack(padx=12, pady=12)
 
 button_right = ctk.CTkButton(master=frame, text="Right", command=login)
-button_right.pack(padx=12, pady=12)
 
 button_bottom = ctk.CTkButton(master=frame, text="Down", command=login)
-button_bottom.pack(padx=12, pady=12)
+
+# Grid layout
+
 
 root.mainloop()
